d12b51e2a4b3369b7672c69aa45474869b3dc36e
pradeep <pradeeprakh2001@gmail.com>
2023-05-02 00:38:18 +0530

    add-two=num

---
+++ ADD-TWO -NUM.js
@@ -0,0 +1,5 @@
+const prompt=require('prompt-sync')()
+let num1=parseInt(prompt("Enter First Number"));
+let num2=parseInt(prompt("Enter Second Number"));
+let sum=num1+num2;
+console.log("Sum of 2 Number is ="+sum);
